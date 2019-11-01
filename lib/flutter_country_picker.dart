d46548d1f4a99073c1dde47f5aaaff0fc6516f0a
aditya sharma <adityaas26@gmail.com>
2019-11-01 15:04:21 +0530

    Fix: Wrap Dialog with padding
    Previously, only the _countryListDialog, which was inside Material widget
    was wrapped with padding widget. So, the padding parameter given by the
    user was not applied when transparentBackground property is true. thus, the
    whole _countryListDialog widget was wrapped inside a Padding widget.

--- lib/flutter_country_picker.dart
+++ lib/flutter_country_picker.dart
@@ -244,87 +244,87 @@ class _CountryPickerDialogState extends State<_CountryPickerDialog> {
     _countryListDialog()
     : Material(
       type: MaterialType.canvas,
-      child: Padding(
-        padding: padding,
-        child: _countryListDialog(),
-      ),
+      child: _countryListDialog(),
     );
   }
 
   Widget _countryListDialog() {
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: MaterialLocalizations.of(context).searchFieldLabel,
-                prefixIcon: Icon(Icons.search),
-                suffixIcon: filter == null || filter == ""
-                    ? Container(
-                        height: 0.0,
-                        width: 0.0,
-                      )
-                    : InkWell(
-                        child: Icon(Icons.clear),
-                        onTap: () {
-                          controller.clear();
-                        },
-                      ),
-              ),
-              controller: controller,
-            ),
-          ),
-          Expanded(
-            child: Scrollbar(
-              child: ListView.builder(
-                itemCount: countries.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Country country = countries[index];
-                  if (filter == null ||
-                      filter == "" ||
-                      country.name
-                          .toLowerCase()
-                          .contains(filter.toLowerCase()) ||
-                      country.isoCode.contains(filter)) {
-                    return InkWell(
-                      child: ListTile(
-                        trailing: Text("+ ${country.dialingCode}"),
-                        title: Row(
-                          children: <Widget>[
-                            showFlagOnList ?
-                            Padding(
-                              padding: EdgeInsets.only(right: 8.0),
-                              child: Image.asset(
-                                country.asset,
-                                package: "flutter_country_picker",
-                              ),
-
-                            )
-                            : Container(),
-                            Expanded(
-                              child: Text(
-                                country.name,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
+    return Padding(
+      padding: padding,
+      child: Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: MaterialLocalizations.of(context).searchFieldLabel,
+                  prefixIcon: Icon(Icons.search),
+                  suffixIcon: filter == null || filter == ""
+                      ? Container(
+                          height: 0.0,
+                          width: 0.0,
+                        )
+                      : InkWell(
+                          child: Icon(Icons.clear),
+                          onTap: () {
+                            controller.clear();
+                          },
                         ),
-                      ),
-                      onTap: () {
-                        Navigator.pop(context, country);
-                      },
-                    );
-                  }
-                  return Container();
-                },
+                ),
+                controller: controller,
               ),
             ),
-          ),
-        ],
+            Expanded(
+              child: Scrollbar(
+                child: ListView.builder(
+                  itemCount: countries.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Country country = countries[index];
+                    if (filter == null ||
+                        filter == "" ||
+                        country.name
+                            .toLowerCase()
+                            .contains(filter.toLowerCase()) ||
+                        country.isoCode.contains(filter)) {
+                      return InkWell(
+                        child: ListTile(
+                          trailing: Text("+ ${country.dialingCode}"),
+                          title: Row(
+                            children: <Widget>[
+                              showFlagOnList ?
+                              Padding(
+                                padding: EdgeInsets.only(right: 8.0),
+                                child: Image.asset(
+                                  country.asset,
+                                  package: "flutter_country_picker",
+                                ),
+
+                              )
+                              : Container(),
+                              Expanded(
+                                child: Text(
+                                  country.name,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pop(context, country);
+                        },
+                      );
+                    }
+                    return Container();
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
